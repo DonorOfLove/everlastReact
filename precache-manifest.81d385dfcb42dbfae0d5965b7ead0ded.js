8c8613473b1a7591ccc191d5b8cbc372ded571c5
DonorOfLove <StasEzovitov89@gmail.com>
2022-03-10 18:32:29 +0300

    Updates

---
+++ precache-manifest.81d385dfcb42dbfae0d5965b7ead0ded.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a70389c8e46a4a1d2e92769d511827",
+    "url": "/everlastReact/index.html"
+  },
+  {
+    "revision": "b8de4a13d32cca3c4eb3",
+    "url": "/everlastReact/static/css/main.c0e5931a.chunk.css"
+  },
+  {
+    "revision": "3056b62bd0889d834f25",
+    "url": "/everlastReact/static/js/2.b9ed46bf.chunk.js"
+  },
+  {
+    "revision": "b8de4a13d32cca3c4eb3",
+    "url": "/everlastReact/static/js/main.94a7b640.chunk.js"
+  },
+  {
+    "revision": "762ca94febf6d3194fa6",
+    "url": "/everlastReact/static/js/runtime~main.321424fd.js"
+  },
+  {
+    "revision": "a2211940be14e086bc7a7c9e5d3ffab8",
+    "url": "/everlastReact/static/media/Fifaks10Dev1.a2211940.ttf"
+  },
+  {
+    "revision": "294e820981f7d48afdb2807ba3129af0",
+    "url": "/everlastReact/static/media/Memphis_atck.294e8209.gif"
+  },
+  {
+    "revision": "69e473a819f10d52a1c20f0504fea442",
+    "url": "/everlastReact/static/media/Memphis_idle.69e473a8.gif"
+  },
+  {
+    "revision": "0ea693be0a1d6f548dc6dd5a2b96b6ab",
+    "url": "/everlastReact/static/media/Skeleton_atck.0ea693be.gif"
+  },
+  {
+    "revision": "189884565cf00d51f9020e0c292dbd3f",
+    "url": "/everlastReact/static/media/Skeleton_idle.18988456.gif"
+  },
+  {
+    "revision": "8904e8a84a4f3c471b68d108406f7a75",
+    "url": "/everlastReact/static/media/Sprite-fantfantasma_idle.8904e8a8.gif"
+  },
+  {
+    "revision": "e830b8118db346887e2c1eb509da2147",
+    "url": "/everlastReact/static/media/campaign.e830b811.png"
+  },
+  {
+    "revision": "72247da01879c802ec924b1af7d4fdf3",
+    "url": "/everlastReact/static/media/defcaleon_atck.72247da0.gif"
+  },
+  {
+    "revision": "3fd6d1393f3435a96065bfd2bdd749f8",
+    "url": "/everlastReact/static/media/defcaleon_idle.3fd6d139.gif"
+  },
+  {
+    "revision": "bc2c12d1d2f87c9b4ce016840fe668cb",
+    "url": "/everlastReact/static/media/everlast.bc2c12d1.gif"
+  },
+  {
+    "revision": "1c07db1c695a14e43982491d03486383",
+    "url": "/everlastReact/static/media/heroes.1c07db1c.gif"
+  },
+  {
+    "revision": "adc4b6eff3ed1d571f4b4c9b4b65b7cb",
+    "url": "/everlastReact/static/media/immortal.adc4b6ef.gif"
+  },
+  {
+    "revision": "74bc04a70257f02f8ed0db5e8eb0748f",
+    "url": "/everlastReact/static/media/immortal_atck.74bc04a7.gif"
+  },
+  {
+    "revision": "577ac35fa0c7d60d808a3fae1e028e39",
+    "url": "/everlastReact/static/media/immortal_idle.577ac35f.gif"
+  },
+  {
+    "revision": "46e0d259b022cfc36637b71f7da00e19",
+    "url": "/everlastReact/static/media/map.46e0d259.gif"
+  },
+  {
+    "revision": "bdba5825df2dcc36b79c8d43c59cfa0c",
+    "url": "/everlastReact/static/media/shugheku_atck.bdba5825.gif"
+  },
+  {
+    "revision": "059e8854f8b158c3f36579a18449e10d",
+    "url": "/everlastReact/static/media/shugheku_idle.059e8854.gif"
+  },
+  {
+    "revision": "cc012d9b117ef608538c0296f960bd45",
+    "url": "/everlastReact/static/media/store.cc012d9b.png"
+  },
+  {
+    "revision": "01d6340c1eb1f7fc785e53766e3ebcde",
+    "url": "/everlastReact/static/media/warrior_atck.01d6340c.gif"
+  },
+  {
+    "revision": "ab2924adb5e22b9b5a055f7c1ba89c93",
+    "url": "/everlastReact/static/media/witch_atck.ab2924ad.gif"
+  }
+]);
